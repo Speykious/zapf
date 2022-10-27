b0104efe5fc66387272a0e74463ecd1c9e666dcc
Speykious <speykious@gmail.com>
2022-10-27 19:02:31 +0200

    Create script for implementation
    Co-authored-by: Steve Gremory <SteveGremory@users.noreply.github.com>

---
+++ src/bin/zapf.rs
@@ -0,0 +1,41 @@
+use clap::Parser;
+use walkdir::WalkDir;
+use zapf::combine_files;
+use std::fs::{File, self};
+use std::io::{self, BufWriter};
+use std::path::{PathBuf};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Cli {
+    pub folder: PathBuf,
+    pub output: Option<PathBuf>,
+}
+
+fn main() -> io::Result<()> {
+    let cli = Cli::parse();
+    let folder = fs::canonicalize(&cli.folder)?;
+    
+
+    println!("Folder: {}", folder.display());
+
+    let mut paths: Vec<PathBuf> = Vec::new();
+    for entry in WalkDir::new(&cli.folder) {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_dir() {
+            continue;
+        }
+
+        paths.push(path.to_owned());
+    }
+
+    let out_path = cli.output.unwrap_or_default().join(folder.file_name().unwrap());
+    dbg!(&out_path);
+    let out_file = File::create(&out_path)?;
+    let mut out_writer = BufWriter::new(out_file);
+
+    combine_files(&folder, &paths, &mut out_writer)?;
+
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,24 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use std::fs::File;
+use std::io::{Write, self};
+use std::path::Path;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// Ideally, it should be given a vector of (fs::Files) and it
+// Reads all the given files and stores their parent agnostic
+// (aka no prefix) paths, size and position in the combined file
+// in its header.
+// The writing of said files to the combined file shall be async in nature
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn combine_files<W: Write>(
+    root_path: impl AsRef<Path>,
+    paths: &[impl AsRef<Path>],
+    writer: &mut W,
+) -> io::Result<()> {
+    for path in paths {
+        let path = path.as_ref();
+        let file = File::open(&path)?;
+
+        let path = path.strip_prefix(&root_path).unwrap();
     }
+
+    Ok(())
 }
